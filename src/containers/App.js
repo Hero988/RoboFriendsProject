c4251ef6b3ab5df74e4761f70443e42bcd7f3f60
Hero988 <gissuliman@gmail.com>
2022-10-18 16:36:02 +0100

    robo Friends project installation

---
+++ src/containers/App.js
@@ -0,0 +1,53 @@
+import React from 'react';
+// leave the folder means ..
+import CardList from '../components/CardList';
+import SearchBox from '../components/SearchBox';
+import Scroll from '../components/Scroll';
+import './App.css'
+
+class App extends React.Component {
+    constructor() {
+      super()
+         this.state = {
+         robots: [],
+         searchfield: ''
+      }
+    }
+
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(Response => Response.json())
+        .then(users => this.setState({ robots: users}));
+    }
+    
+    onSearchChange = (event) => {
+        this.setState({ searchfield: event.target.value });
+
+    }
+
+    render() {
+          const { robots, searchfield} = this.state;
+          const filteredRobots = robots.filter(robot => {
+         return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+       })
+       if(!robots.length) {
+        return <h1> loading</h1>
+       } else {
+        return (
+            <div className='tc'>
+                <h1 className='f1'>RoboFriends</h1>
+                <SearchBox searchChange={this.onSearchChange}/>
+               <Scroll>
+                  <CardList robots={filteredRobots}/>
+               </Scroll>
+            </div>
+           )
+       }
+    }
+
+}
+export default App;
+
+// STATE means the description of the app (able to change)
+// PROPS are simply things that came out of STATE (not able to change)
+// STATE >> PROPS
