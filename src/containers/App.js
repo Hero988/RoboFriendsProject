14c52a843934cc4ca34bbcc3ce7d751314113b69
Hero988 <gissuliman@gmail.com>
2022-10-18 18:18:10 +0100

    new files

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
-// leave the folder means ..
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
+import ErrorBoundry from '../components/ErrorBoundry';
 import './App.css'
 
 class App extends React.Component {
@@ -38,7 +38,9 @@ class App extends React.Component {
                 <h1 className='f1'>RoboFriends</h1>
                 <SearchBox searchChange={this.onSearchChange}/>
                <Scroll>
-                  <CardList robots={filteredRobots}/>
+                  <ErrorBoundry>
+                     <CardList robots={filteredRobots}/>
+                  </ErrorBoundry>
                </Scroll>
             </div>
            )
